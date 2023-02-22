7e9a6cb207571bb175920a05b1aaa5741cd2a69f
Sa-Neto <sa.jrneto@gmail.com>
2023-02-22 20:17:17 -0300

    cronometro

--- App.js
+++ App.js
@@ -1,11 +1,84 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+let timer = null;
+let ss = 0;
+let mm = 0;
+let hh = 0;
 
 export default function App() {
+  const [numero, setNumero] = useState(0);
+  const [botao, setBotao] = useState('Iniciar');
+  const [ultimo, setUltimo] = useState();
+  function vai(){
+    if(timer !== null ){
+      //Aqui vai parar o timer
+      clearInterval(timer);
+      timer = null;
+
+      setBotao('Voltar');
+    }else{
+      timer = setInterval(() => {
+        ss++;
+
+        if(ss == 60){
+          ss = 0
+          mm ++;
+        }
+
+        if(mm == 60){
+          mm = 0;
+          hh++;
+        }
+
+        let format =
+        (hh < 10? '0' + hh: hh) + ':' + (mm < 10 ? '0' + mm : mm ) + ':' + (ss < 10 ? '0' + ss : ss) ;
+        setNumero(format)
+        
+      },1000)
+      setBotao('Parar')
+    }
+  }
+
+  function limpar(){
+    if(timer !== null){
+      //Parar o timer!
+      clearInterval(timer);
+      timer = null;
+    }
+    setUltimo(numero)
+    setNumero(0);
+    ss = 0;
+    mm = 0;
+    hh = 0;
+    setBotao('Vai');
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image 
+        source={require('./src/crono.png')}
+      />
+      <Text style={styles.timer}>{numero}</Text>
+      
+      <View style={styles.btnArea}>
+        
+        <TouchableOpacity style={styles.btn} onPress={vai}>
+          <Text style={styles.btnTexto}>{botao}</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btn} onPress={limpar}>
+          <Text style={styles.btnTexto}>Limpar</Text>
+        </TouchableOpacity>
+
+      </View>
+
+      <View style={styles.areaUltima}>
+          <Text style={styles.textoCorrida}>
+            {ultimo ? 'Ultimo Tempo:' + ultimo : ''}
+          </Text>
+      </View>
+
     </View>
   );
 }
@@ -13,8 +86,43 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#00aeef'
   },
+  timer:{
+    marginTop: -160,
+    fontSize:45,
+    fontWeight:'bold',
+    color:'#FFF'
+  },
+  btnArea:{
+    flexDirection:'row',
+    marginTop:130,
+    height:40,
+  },
+  btn:{
+    flex:1,
+    justifyContent:'center',
+    alignItems: 'center',
+    backgroundColor: '#FFF',
+    height:40,
+    margin:17,
+    borderRadius:9
+  },
+
+  btnTexto:{
+    fontSize:20,
+    fontWeight:'bold',
+    color: '#00aeef'
+  },
+  areaUltima:{
+    marginTop:40
+  },
+  textoCorrida:{
+    fontSize:25,
+    color: '#fff',
+    fontStyle: 'italic'
+  }
+
 });
---
+++ readme.md
@@ -0,0 +1,12 @@
+<h1>Cronometro</h1>
+
+<h3>Stacks usadas</h3>
+<ul> 
+  <li>React-Native</li>
+  <li>Expo</li>
+</ul>
+<p>Um aplicativos de cronômetro, que tem duas funções, limpar, iniciar e salva o ultimo tempo.Usando o javascript puro.<p>
+
+<img src='./assets/cro1.png'/>
+<img src='./assets/cro2.png'/>
+<img src='./assets/cro3.png'/>
